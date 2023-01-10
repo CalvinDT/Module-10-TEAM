e66ce3eaeb5c18107b4dad808868f1a247d7c19e
CalvinDT <ct223344@gmail.com>
2023-01-09 21:19:34 -0500

    updated classes

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const render = require("./lib/htmlRenderer");
 
 const team = []
 
-// Employees
+// Employee
 const whatToDo = () => {
   inquirer
     .prompt([
       {
         type: 'list',
-        message: 'Which employee would you like to add?',
+        message: 'What team member would you like to add?',
         name: 'members',
         choices: ["Engineer", "Intern", "Quit"]
       },
@@ -31,7 +31,11 @@ const whatToDo = () => {
           if (err) throw err;
           console.log('File Was Created');
         });
-        
+        // fs.writeFile(outputPath, render(team),(err)=>{
+        //   if(err) throw err;
+        //   console.log("team created")
+        //   return
+        // })
       }
       else if (members === "Engineer") {
         makeEngineer()
@@ -42,7 +46,32 @@ const whatToDo = () => {
 
     })
 }
+//   inquirer
+//     .prompt([
+//       {
+//         type: 'input',
+//         message: 'What is your user name?',
+//         name: 'username',
+//       },
+//       {
+//         type: 'input',
+//         message: 'What is your id?',
+//         name: 'id',
+//       },
+//       {
+//         type: 'input',
+//         message: 'What is your email?',
+//         name: 'email',
+//       },
+//     ])
+//     .then((response) => {
+//       let newEmployee = new Employee(response.username, response.id, response.email);
+//       team.push(newEmployee);
 
+//     }
+//     );
+//   }
+// });
 
 
 //  Engineer
@@ -62,7 +91,7 @@ const makeEngineer = () => {
       },
       {
         type: 'input',
-        message: 'What is your primary email?',
+        message: 'What is your email?',
         name: 'email',
       },
       {
@@ -94,7 +123,7 @@ const makeIntern = () => {
       },
       {
         type: 'input',
-        message: 'What is your primary email?',
+        message: 'What is your email?',
         name: 'email',
       },
       {
@@ -132,7 +161,7 @@ const makeManager = () => {
       },
       {
         type: 'input',
-        message: 'What is your office number?',
+        message: 'What is your officeNumber?',
         name: 'officeNumber',
       },
     ])
@@ -143,3 +172,36 @@ const makeManager = () => {
     });
 }
  makeManager()
+// function init() {
+//   inq.prompt(team).then((response) => {
+//     console.log(response)
+//     fs.writeFile('team.html')
+//   });
+// }
+
+// init();
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
